67abfdf2b2bb7a8ab4ddd0f083d10df256b0ddd6
Jain7 <RJain7@ad.harman.com>
2019-02-22 13:59:04 +0530

    Added expect assertion library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,19 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('Should add 2 numbers', () => {
     let res = utils.add(22, 11);
-    if(res !== 33) {
-        throw new Error(`Answer should be 33 but got ${res}`);
-    }
+    // if(res !== 33) {
+    //     throw new Error(`Answer should be 33 but got ${res}`);
+    // }
+    expect(res).toBe(33).toBeA('number');
 });
 
 it('Should sqaure a number', () => {
     let res = utils.square(5);
-    if(res !== 25) {
-        throw new Error(`Result should be 25 but got ${res}`)
-    }
+    // if(res !== 25) {
+    //     throw new Error(`Result should be 25 but got ${res}`)
+    // }
+    expect(res).toBe(25).toBeA('number');
 });
